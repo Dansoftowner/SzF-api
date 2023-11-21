66f292e4c13e557644cad9d14fa8dda58557d269
Györffy.Dániel <judalion777@gmail.com>
2023-11-21 09:31:09 +0100

    :sparkles: Get training by id

--- controllers/trainings.js
+++ controllers/trainings.js
@@ -1,13 +1,27 @@
+const mongoose = require('mongoose');
 const Training = require('../models/training');
 
-exports.getTrainings = (req, res, next) => {
-  res
-    .status(200)
-    .json({ success: true, msg: 'Show all trainings', hello: req.hello });
+exports.getTrainings = async (req, res, next) => {
+  try {
+    const trainings = await Training.find({});
+    res.status(200).json(trainings);
+  } catch (err) {
+    res.status(500).json({ success: false });
+  }
 };
 
-exports.getTraining = (req, res, next) => {
-  res.status(200).json({ success: true, msg: `Get training ${req.params.id}` });
+exports.getTraining = async (req, res, next) => {
+  try {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id))
+      return res.status(400).json({ success: false });
+
+    const training = await Training.findById(req.params.id);
+    if (!training) return res.status(404).json({ success: false });
+
+    res.status(200).json(training);
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 exports.createTraining = async (req, res, next) => {
